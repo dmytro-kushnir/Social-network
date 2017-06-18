8c740eb1da0059f85e333970e9ca5ebd6d28f356
Dima <dimakush1@gmail.com>
2017-06-18 10:45:04 +0300

    ng-route to ui-route migrate. Change paths from locall to CDN

--- app/controllers/navigation.controller.js
+++ app/controllers/navigation.controller.js
@@ -1,6 +1,8 @@
 var app = angular.module("Controllers", []);
 
 app.controller('RouterCtrl' , [function(){
+
+    console.log(this);
 //    var vm = this;
 //    vm.test = "test";
 //    vm.menuItems = [

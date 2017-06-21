bdd28f8ee923ddda6f12c9e720f30fb7c368f2a5
Anya2212 <medvidanja94@gmail.com>
2017-06-22 00:17:48 +0300

    del file registration.js
    del file registration.js

--- app/controllers/registration.js
+++
@@ -1,6 +0,0 @@
-var  singUp = angular.module("singUp", ["ngRoute"]).controller("singUpController", function($scope, $http){
-    $scope.addNewUser = function(userDetails){
-        $scope.massage = userDetails.surname + userDetails.name + "("+userDetails.email+")" + "("+userDetails.password+")";
-    }
-    $scope.message = "Ready";
-});

47e8568ddaa862f51decb98072fb0a831ad1c71e
Dima <dimakush1@gmail.com>
2017-06-23 10:48:40 +0300

    little fixes after ui-route migrating

--- app/app.js
+++ app/app.js
@@ -5,13 +5,4 @@
     app.controller('MainCtrl', function () {
 
     });
-
-
-    var app2 = angular.module("singupApp", []);
-
-
-    var app3 = angular.module("mainContainer", ['ui.router']).controller('containerCtrl', function(){
-
-    });
-   
 })();

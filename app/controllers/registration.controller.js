643bfb163111c741483dca05dc44b712341a2a32
Anna <medvidanja94@gmail.com>
2017-07-01 17:24:29 +0300

    add user.json

--- app/controllers/registration.controller.js
+++ app/controllers/registration.controller.js
@@ -1,5 +1,4 @@
 //var app = angular.module("socialNetwork");
-
 app.controller("singUpController", function($scope){
 	$scope.register = function(newUser){
 		$scope.msg = 'Welcome ' +$scope.user.secondname+'! You have singed in =)';

1057bd27e2205f818a41f84625e04c8d66b19548
Dima <dimakush1@gmail.com>
2017-07-14 12:07:48 +0300

    solved problem with overloading

--- app/controllers/chatUser.controller.js
+++ app/controllers/chatUser.controller.js
@@ -1,7 +1,12 @@
-app.controller("chatUserCtrl",[ '$scope', '$window' , function($scope, $window){
+app.controller("chatUserCtrl", function($state, $scope, $window){
+
+if (performance.navigation.type == 1) { // if page reload
+    $state.go("mainContainer.chat",{},{reload: "mainContainer.chat"});
+}
+
     var width = $window.innerWidth;
     if(width < 767){
         $scope.tabletMode = true;
     }
     
-}]);
+});
--- app/controllers/mainPage.controller.js
+++ app/controllers/mainPage.controller.js
@@ -1,45 +1,71 @@
-// rendering page controller
-app.controller('MainCtrl', function ($rootScope, $scope, JsonLoad, JsonFriend) {
-    'use strict';
+(function () {
 
-    // Завантаження сторінки 
-    JsonLoad.getPage().then(function (res) {
-        console.log(res);
-        $scope.page = [];
-        $scope.page = res.data;
-        $scope.carouselIndex = 1; // щоб сладйер починався з другого індексу
+
+    // rendering page controller
+    app.controller('MainCtrl', function ($rootScope, $scope, JsonLoad, JsonFriend, storageService) {
+
+        'use strict';
+
+
+
+        // return to main USER page onclick
+        $scope.return = function (answer) {
+            JsonLoad.returnHome(answer).then(function (res) {
+                $scope.page = res.data;
+                console.log("mainPage POST", res.data);
+                $scope.carouselIndex = 1; // щоб сладйер починався з другого індексу
+            });
+        }
+
+        // load userPage onclick
+        $scope.reqFriend = function (answer) {
+            // save friendId to localStorage
+            storageService.save('friendId', answer);
+        }
+
+        // load chatUser onclick 
+        $scope.chatEnter = function (chatId) {
+            $scope.chatId = chatId;
+            console.log(`chat index is ${$scope.chatId}`);
+            $rootScope.chatTitle = $scope.page.chat[chatId].chatName;
+        };
+
+
+        $scope.$on('friend', function (event, data) { // get data from child ctrl userPage
+            $scope.page = data;
+            console.log("emit data FRIEND", $scope.page);
+            $scope.hey = data;
+
+        });
+        $scope.$on('user', function (event, data) { //get data from child ctrl and add it to LS
+            storageService.save('mainUser',JSON.stringify(data));
+        });
+
+        if (performance.navigation.type == 1) { // if page reload
+          
+            var userData=storageService.get('mainUser'); //get userData to scope
+             $scope.page = JSON.parse(userData);
+        }
+
     });
 
-    // повернення до розділв користувача по кліку
-      $scope.return = function(answer){
-          JsonLoad.returnHome(answer).then(function(res) {
+    app.controller('MainPageCtrl', function ($rootScope, $scope, JsonLoad, storageService) {
+        'use strict';
+        // load main USER page
+        JsonLoad.getPage().then(function (res) {
+            console.log("mainPage GET", res);
+            $scope.page = [];
             $scope.page = res.data;
-            console.log(res.data);
+
+            storageService.save('userName', res.data.firstName + " " + res.data.secondName);
+
             $scope.carouselIndex = 1; // щоб сладйер починався з другого індексу
-        });
-      }
 
-
-    // Завантаження сторінки друга при кліку 
-    $scope.response = {};
-    $scope.reqFriend = function (answer) {
-       
-        // console.log(`friend index is ${$scope.friendId}`);
-        JsonFriend.requestPage(answer).then(function(res) {
-            $scope.friend = res.data;
-            $scope.page = $scope.friend;
-            console.log(res.data);
-            $rootScope.friendName = $scope.friend.firstName;
-            // $rootScope.friendId = $scope.friend.id;
-                       
+            $scope.$emit('user', res.data);
+            storageService.save('user', res.data);
         });
 
-    }
-    
-    // Функція входження в чат по індексу
-    $scope.chatEnter = function (chatId) {
-        $scope.chatId = chatId;
-        console.log(`chat index is ${$scope.chatId}`);
-        $rootScope.chatTitle = $scope.page.chat[chatId].chatName;
-    };
-});
+
+    });
+
+})();

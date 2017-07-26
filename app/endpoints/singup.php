f48d969c8034435658e84266a938f24fe92bf05c
Anna <medvidanja94@gmail.com>
2017-07-26 11:20:01 +0300

    add DB + registration users

--- app/endpoints/singup.php
+++ app/endpoints/singup.php
@@ -1,6 +1,3 @@
 <?php
-	include("../connection.php");
-	$data = json_decode(file_get_contents("php://input"));
-
-	echo json_encode($data);
+	include("../../axaj.php");
 ?>
---
+++ users.sql
@@ -0,0 +1,65 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.5.2
+-- https://www.phpmyadmin.net/
+--
+-- Хост: 127.0.0.1
+-- Час створення: Лип 26 2017 р., 10:12
+-- Версія сервера: 10.1.21-MariaDB
+-- Версія PHP: 5.6.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- База даних: `social_network`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблиці `users`
+--
+
+CREATE TABLE `users` (
+  `userId` int(11) UNSIGNED NOT NULL,
+  `userFirstName` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
+  `userSecondName` varchar(40) COLLATE utf8_unicode_ci NOT NULL,
+  `userEmail` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `userPassword` varchar(100) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Дамп даних таблиці `users`
+--
+
+INSERT INTO `users` (`userId`, `userFirstName`, `userSecondName`, `userEmail`, `userPassword`) VALUES
+(1, 'test', 'test', 'test@test_om', '123456');
+
+--
+-- Індекси збережених таблиць
+--
+
+--
+-- Індекси таблиці `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`userId`);
+
+--
+-- AUTO_INCREMENT для збережених таблиць
+--
+
+--
+-- AUTO_INCREMENT для таблиці `users`
+--
+ALTER TABLE `users`
+  MODIFY `userId` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;

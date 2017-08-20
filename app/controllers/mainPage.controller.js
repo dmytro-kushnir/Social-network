e256955103fff2afa81dfa64c148e37e635cfd58
Dima <dimakush1@gmail.com>
2017-08-20 23:26:54 +0300

    mainPage post addition COMPLETE

--- app/controllers/mainPage.controller.js
+++ app/controllers/mainPage.controller.js
@@ -4,8 +4,20 @@
         $scope.textAreaFlag = true;// for post send validation 
         $scope.imageFlag = true;
        console.log($scope.textarea);
-$scope.textarea = {};
-        $scope.uploadPic = function (file) { 
+        $scope.textarea = {};
+        $scope.uploadPic = function (file) {
+            var date = new Date();
+            var data = { // prepare data to server send
+                "id_owner": storageService.get("userId"),
+                "sender_url":"../src/img/users/user/avatars/2.jpg",
+                "sender_name":storageService.get("userName"), 
+                "send_date": dateFormat(new Date(), 'm-d-Y h:i:s'), 
+                "post_text":$scope.textarea.value, 
+                "post_link":"", // make in server
+                "post_image":"../src/img/users/user/posts/", //make in server
+                "post_likes":0 
+            }
+            
             if(file != undefined || file != null){ //text with file or only file
             file.upload = Upload.upload({
                 url: 'endPoints/upload-image.php',
@@ -13,16 +25,17 @@ $scope.textarea = {};
                 file: file,
                 data: {
                     // 'targetPath' : '/src/img/users/user'+$scope.page.id+'/posts/',
-                    'targetPath' : '../src/img/users/user/posts/',
-                    'textArea': $scope.textarea,
-                    'file': file
+                    "dataArr": data,
+                    "file": file
                 }
             });
 
             file.upload.then(function (response) {
                 $timeout(function () {
                     console.log("FILE RESULT", response.data);
-                    file.result = response.data;
+                    $scope.page.posts.push(response.data.info);
+                    console.log("ADD POST", $scope.page);
+                    // file.result = response.data;
                 });
             }, function (response) {
                 if (response.status > 0)
--- endPoints/upload-image.php
+++ endPoints/upload-image.php
@@ -5,5 +5,20 @@ $Db = new \Db\Db();
 
   $filename = $_FILES['file']['name'];
   $meta = $_POST;
-  $destination = $meta['targetPath'] . $filename;
+  $destination = $meta['dataArr']['post_image'] . $filename;
+  $data = $meta['dataArr'];
+  
+  $data['post_image'] = $destination;
+
   move_uploaded_file( $_FILES['file']['tmp_name'] , $destination );
+
+  $insertId = $Db->addSql('post', $data); // add post to SQL
+ 
+
+  $result = [
+    'info' => $data,
+    'errors' => []
+];
+
+header('Content-Type: application/json');
+echo json_encode($result);

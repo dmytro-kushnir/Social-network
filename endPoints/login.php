0f80a97ad70e6889afbdbd323f6b870d50913077
Dima <dimakush1@gmail.com>
2017-09-07 19:55:15 +0300

    reset

--- endPoints/login.php
+++ endPoints/login.php
@@ -2,14 +2,18 @@
 include ('../Config/config.php');
 include ('../Db/Db.php');
 $Db = new \Db\Db();
-
   $data = json_decode(file_get_contents('php://input'), true);
   $select = "SELECT count(*) as counter FROM users WHERE userEmail = '$data[userEmail]' AND userPassword = '$data[userPassword]'";
   $query = $Db->query($select);
   $query->execute();
   $num = $query->fetchAll(PDO::FETCH_ASSOC);
   $userInfo = null;
-
+  $id = null;
+if (!empty($data)) {
+    if ($num[0]['counter'] > 0) {
+        $id = 1; //АНЯ.  знайди айдішку по логінуванню і передавай сюди
+        $success = true;
+    ///////////////////////////////////
     } else {
         $userInfo = "Користувача з таким емейлом, або паролем не існує";
         $success= false;
@@ -21,6 +25,5 @@ $Db = new \Db\Db();
         'userInfo'=>$userInfo,
         'errors' => []
     ];
-
     header('Content-Type: application/json');
     echo json_encode($result);

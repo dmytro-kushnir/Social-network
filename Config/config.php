0b2e38211710a011bb9fd180aa0c296f4ab8aafe
Dima <dimakush1@gmail.com>
2018-02-11 22:19:21 +0200

    revert to workable status

--- Config/config.php
+++ Config/config.php
@@ -14,18 +14,18 @@ spl_autoload_register(function ($class_name) {
 
 // localhost
 
-define('DB_HOST', "localhost");
-define('DB_PORT', '3306');
-define('DB_SCHEMA', "social_network");
-define('DB_LOGIN', "root");
-define('DB_PASSWORD', "root");
-define('DB_UTC', "+2:00");
+ define('DB_HOST', "localhost");
+ define('DB_PORT', '3306');
+ define('DB_SCHEMA', "social_network");
+ define('DB_LOGIN', "root");
+ define('DB_PASSWORD', "root");
+ define('DB_UTC', "+2:00");
 
 // hostinger
 
-// define('DB_HOST', 'mysql.hostinger.com.ua');
-// define('DB_PORT', '3306');
-// define('DB_SCHEMA', 'u579067471_soc');
-// define('DB_LOGIN', 'u579067471_dima');
-// define('DB_PASSWORD', '1234qwerty'); 
-// define('DB_UTC', '+2:00');
+//define('DB_HOST', 'mysql.hostinger.com.ua');
+//define('DB_PORT', '3306');
+//define('DB_SCHEMA', 'u579067471_soc');
+//define('DB_LOGIN', 'u579067471_dima');
+//define('DB_PASSWORD', '1234qwerty');
+//define('DB_UTC', '+2:00');
